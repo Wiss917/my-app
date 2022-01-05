79d26283df59706814471d797313aec53a06e2e7
wuzixuan <wisszeix@foxmail.com>
2022-01-05 16:09:13 +0800

    :sparkles: 新增登录功能

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,15 +1,7 @@
-/*
- * @Author: your name
- * @Date: 2022-01-01 13:05:55
- * @LastEditTime: 2022-01-01 14:17:37
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \my-app-1\src\api\login.ts
- */
 import instance from '@/utils/axios';
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from 'axios';
+import qs from 'qs';
 
-type UserInfo = {
+export type UserInfo = {
   tenantId: string;
   username: string;
   password: string;
@@ -25,13 +17,10 @@ export interface IResponse<T = any> {
   success: boolean;
 }
 
-type UserToken = {
-  data: object;
-  // userToken: string;
-};
-
-export function getUserInfo(data: UserInfo): Promise<IResponse<UserToken>> {
-  return instance.post('/blade-auth/oauth/token', {
-    data,
+export function getUserInfo(data: UserInfo): Promise<{ access_token: string }> {
+  return instance.post('/blade-auth/oauth/token', qs.stringify(data), {
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    }
   });
 }
